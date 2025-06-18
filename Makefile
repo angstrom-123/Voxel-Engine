2ed38a1debd63e2e4c130a52e0502e93d6964876
angstrom-123 <ignacy.wegner+1@gmail.com>
2025-06-18 21:57:47 +0100

    setup sokol shaders

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 CFILES	= $(wildcard ./src/*.c)
+SHADERS	= $(wildcard ./src/*.glsl)
+
 CC		= clang
 CFLAGS	= -Wall -Wextra -pthread
 INCLUDE	= -I./lib -I.
@@ -13,7 +15,11 @@ install: $(CFILES)
 .PHONY: compile-database
 compile-database: $(CFILES)
 	bear -- $(CC) $(CFILES) -o $(TARGET) $(INCLUDE) $(DEFS) $(CFLAGS) $(LX_LIBS) 
+
+.PHONY: compile-shaders
+compile-shaders: $(SHADERS)
+	$(foreach shdr, $(SHADERS), ./sokol-shdc -i $(shdr) -o $(shdr).h -l glsl300es)
 	
 .PHONY: clean
-clean:
+clean: 
 	rm -f $(TARGET)
--- src/main.c
+++ src/main.c
@@ -4,28 +4,61 @@
 #include "sokol_app.h"
 #include "sokol_glue.h"
 
-#include <stdio.h>
+#include "triangle.glsl.h"
+
+static struct {
+	sg_pipeline pip;
+	sg_bindings bind;
+	sg_pass_action pass_action;
+} state;
 
 static void init(void)
 {
-	sg_desc desc = {
+	sg_setup(&(sg_desc) {
 		.environment = sglue_environment(),
+	});
+
+	float vertices[] = {
+		 0.0,  0.5, 0.5,	1.0, 0.0, 0.0, 1.0,
+		 0.5, -0.5, 0.5,	0.0, 1.0, 0.0, 1.0,
+		-0.5, -0.5, 0.5, 	0.0, 0.0, 1.0, 1.0
 	};
-	sg_setup(&desc);
+
+	state.bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc) {
+		.data = SG_RANGE(vertices),
+		.label = "triangle-vertices",
+	});
+
+	sg_shader shdr = sg_make_shader(triangle_shader_desc(sg_query_backend()));
+
+	state.pip = sg_make_pipeline(&(sg_pipeline_desc) {
+		.shader = shdr,
+		.layout = {
+			.attrs = {
+				[ATTR_triangle_position].format = SG_VERTEXFORMAT_FLOAT3,
+				[ATTR_triangle_color0].format = SG_VERTEXFORMAT_FLOAT4
+			},
+		}, 
+		.label = "triangle-pipeline",
+	});
+
+	state.pass_action =(sg_pass_action) {
+		.colors[0] = {
+			.load_action = SG_LOADACTION_CLEAR,
+			.clear_value = {0.0, 0.0, 0.0, 1.0}
+		}
+	};
 }
 
 static void frame(void)
 {
-	sg_pass pass = {
-		.action = {
-			.colors[0] = {
-				.load_action = SG_LOADACTION_CLEAR,
-				.clear_value = {0.0, 0.5, 1.0, 1.0},
-			},
-		},
-		.swapchain = sglue_swapchain(),
-	};
-	sg_begin_pass(&pass);
+	sg_begin_pass(&(sg_pass) {
+		.action = state.pass_action,
+		.swapchain = sglue_swapchain()
+	});
+	sg_apply_pipeline(state.pip);
+	sg_apply_bindings(&state.bind);
+	sg_draw(0, 3, 1);
 	sg_end_pass();
 	sg_commit();
 }
@@ -37,13 +70,11 @@ static void cleanup(void)
 
 static void event(const sapp_event* event)
 {
-	printf("event: %u\n", event->char_code);
+	(void) event;
 }
 
 sapp_desc sokol_main(int argc, char* argv[])
 {
-	printf("Running\n");
-
 	(void) argc;
 	(void) argv;
 
@@ -52,8 +83,8 @@ sapp_desc sokol_main(int argc, char* argv[])
 		.frame_cb = frame,
 		.cleanup_cb = cleanup,
 		.event_cb = event,
-		.width = 800,
-		.height = 600,
+		.width = 1280,
+		.height = 720,
 		.window_title = "Minecraft Remake",
 		.icon.sokol_default = true,
 	};
