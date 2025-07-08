29b48990b49256dcbd67f283c9634421658044af
angstrom-123 <ignacy.wegner+1@gmail.com>
2025-07-08 16:38:36 +0100

    add all mipmap levels

--- src/state.c
+++ src/state.c
@@ -45,7 +45,7 @@ void state_init_bindings(state_t *state)
 			.mipmap_filter = SG_FILTER_NEAREST,
 			.wrap_u = SG_WRAP_CLAMP_TO_EDGE,
 			.wrap_v = SG_WRAP_CLAMP_TO_EDGE,
-			.max_lod = 1.0,
+			.max_lod = 5.0, /* NOTE: Adjust this to be number of mipmaps */
 		}),
 		.images[0] = sg_alloc_image()
 	};
@@ -56,14 +56,17 @@ void state_init_textures(state_t *state)
 	/* Full-res atlas and mipmap levels */
 	bmp_image_t *atlas = bmp_load_file("res/minecraft_remake_texture_atlas.bmp");
 	bmp_image_t *atlas_mm1 = bmp_load_file("res/minecraft_remake_texture_atlas-mm1.bmp");
+	bmp_image_t *atlas_mm2 = bmp_load_file("res/minecraft_remake_texture_atlas-mm2.bmp");
+	bmp_image_t *atlas_mm3 = bmp_load_file("res/minecraft_remake_texture_atlas-mm3.bmp");
+	bmp_image_t *atlas_mm4 = bmp_load_file("res/minecraft_remake_texture_atlas-mm4.bmp");
 
-	if (atlas && atlas_mm1)
+	if (atlas && atlas_mm1 && atlas_mm2 && atlas_mm3 && atlas_mm4)
 	{
 		sg_init_image(state->bind.images[0], &(sg_image_desc) {
 			.width = atlas->info_header.width,
 			.height = atlas->info_header.height,
 			.pixel_format = SG_PIXELFORMAT_RGBA8,
-			.num_mipmaps = 2,
+			.num_mipmaps = 5,
 			.data.subimage[0][0] = {
 				.ptr = atlas->pixel_data,
 				.size = (size_t) atlas->info_header.img_size
@@ -71,18 +74,39 @@ void state_init_textures(state_t *state)
 			.data.subimage[0][1] = {
 				.ptr = atlas_mm1->pixel_data,
 				.size = (size_t) atlas_mm1->info_header.img_size
-			}
+			},
+			.data.subimage[0][2] = {
+				.ptr = atlas_mm2->pixel_data,
+				.size = (size_t) atlas_mm2->info_header.img_size
+			},
+			.data.subimage[0][3] = {
+				.ptr = atlas_mm3->pixel_data,
+				.size = (size_t) atlas_mm3->info_header.img_size
+			},
+			.data.subimage[0][4] = {
+				.ptr = atlas_mm4->pixel_data,
+				.size = (size_t) atlas_mm4->info_header.img_size
+			},
 		});
 
 		free(atlas->pixel_data);
 		free(atlas_mm1->pixel_data);
+		free(atlas_mm2->pixel_data);
+		free(atlas_mm3->pixel_data);
+		free(atlas_mm4->pixel_data);
 		free(atlas);
 		free(atlas_mm1);
+		free(atlas_mm2);
+		free(atlas_mm3);
+		free(atlas_mm4);
 	} 
 	else 
 	{
 		free(atlas);
 		free(atlas_mm1);
+		free(atlas_mm2);
+		free(atlas_mm3);
+		free(atlas_mm4);
 		fprintf(stderr, "Failed to load texture atlas or mipmaps.");
 		exit(1);
 	}
